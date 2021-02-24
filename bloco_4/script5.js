8be3154974e323c174e5e51ff887666963157e5d
Felippe Correa <felippecorrea2010@gmail.com>
2021-02-24 10:11:47 -0300

    Adicionando exercicio da parte V do desafio de aula

---
+++ bloco_4/script5.js
@@ -0,0 +1,13 @@
+let candidateCondicion = "Reprovadoss";
+
+switch (candidateCondicion) {
+  case "Aprovado":
+    console.log("Parabéns, você foi aprovado!!");
+    break;
+  case "Reprovado":
+    console.log("Você foi reprovado!!");
+    break;
+  default:
+    console.log("Valor não identificado");
+    break;
+}
