ecff1d4a58e5e08ed76987025a9127335528da37
Felippe Correa <felippecorrea2010@gmail.com>
2021-02-24 16:21:20 -0300

    adicionando pasta dos programas criados

---
+++ bloco_4/exercícios/scriptE2.js
@@ -0,0 +1,10 @@
+let numberOne = 2;
+let numberTwo = 5;
+
+if (numberOne > numberTwo) {
+  console.log(numberOne);
+}
+else{
+  console.log(numberTwo);
+}
+
---
+++ bloco_4/exercícios/scriptE3.js
@@ -0,0 +1,13 @@
+let numberOne = 10;
+let numberTwo = 5;
+let numberThree = 20;
+
+if (numberOne > numberTwo && numberOne > numberThree) {
+  console.log(numberOne);
+}
+else if (numberTwo > numberOne && numberTwo > numberThree) {
+  console.log(numberTwo);
+}
+else {
+  console.log(numberThree);
+}
