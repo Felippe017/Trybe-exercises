16b0854fe4cd33a33eb96fabfaee726eb7b468f1
Felippe Correa <felippecorrea2010@gmail.com>
2021-02-24 15:19:00 -0300

    quinto programa dos exercicios do bloco 4.1

---
+++ bloco_4/scriptE5.js
@@ -0,0 +1,11 @@
+let anguloOne = 90;
+let anguloTwo = 30;
+let anguloThree = 80;
+const somaDosAngulos = anguloOne + anguloTwo + anguloThree;
+
+if (somaDosAngulos == 180) {
+  console.log("true");
+}
+else {
+  console.log("false");
+}
