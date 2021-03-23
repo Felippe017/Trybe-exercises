a352cee0c85921a80e0c6d94085b2994cad06005
Felippe Correa <felippecorrea2010@gmail.com>
2021-03-23 15:04:00 -0300

    Adicionando exercicios parte I do bloco 7

---
+++ bloco_7/script.js
@@ -0,0 +1,31 @@
+// if (true) {
+//   // inicio do escopo do if
+//   const userAge = "20";
+//   console.log(userAge); // 20
+//   // fim do escopo do if
+// }
+// console.log(userAge); // 20
+
+// let userName = "Isabella";
+// let userName = "Lucas";
+// console.log(userName); // Resultado: Lucas
+
+// const favoriteLanguage = "Javascript";
+// favoriteLanguage = "Python";
+// console.log(favoriteLanguage); // Erro
+
+// let favoriteTechnology = "Machine learning";
+// favoriteTechnology = "Facial recognition";
+// console.log(favoriteTechnology); // Facial recognition
+
+// // Com o template literals
+// console.log(`Primeira linha;
+// Segunda linha;
+// Terceira linha;`
+// )
+
+// // Sem o template literals:
+// console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n')
+
+const myName = "Isabella"
+console.log(`Welcome ${myName}!`);
