ad33ed991faa856c9a52555b5cbc215ae0bf81ad
Felippe Correa <felippecorrea2010@gmail.com>
2021-05-05 15:30:38 -0300

    adicionando exercicios de aula bloco 13.1

---
+++ bloco_13/my-counter/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Counter from './Counter';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <Counter />
+      </main>
+    );
+  }
+}
+
+export default App;
